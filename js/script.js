f74618502a9d43f058f92df85c149ce9cdca730e
ced-77 <=>
2017-03-09 01:07:01 +0100

    debut du jquery pour traitement formulaire par PHP

---
+++ js/script.js
@@ -0,0 +1,19 @@
+$(document).ready(function{
+
+	// recuperation du formulaire en cliquant sur le submit
+		$('submit').on(click, function(event){
+
+
+
+
+
+		// fin de la fonction du click sur le bouton submit
+		})
+
+
+
+
+
+
+// fin du script javaScript et jQuery
+})
